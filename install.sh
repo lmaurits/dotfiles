eff196fada8024ce6e9402073aae68042c932bf0
Luke Maurits <luke@maurits.id.au>
2014-06-12 14:24:12 +1200

    Simple script to build symlinks to install these dotfiles.

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Bash
+ln -s ~/dotfiles/bash/bash_aliases ~/.bash_aliases
+
+# Screen
+ln -s ~/dotfiles/screen/screenrc ~/.screenrc
+
+# Vim
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+mkdir -p ~/.vim/colors/
+ln -s ~/dotfiles/vim/solarized.vim ~/.vim/colors/solarized.vim
+
+# X
+ln -s ~/dotfiles/X/Xresources ~/.Xresources
